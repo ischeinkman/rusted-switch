7183e03207d91a31a90c6aa538c05872917436a3
ischeinkman <scheinkman.ilan@gmail.com>
2018-07-24 22:49:33 -0700

    Moved libnx bindings to separate module.

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,11 @@
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
+#![allow(dead_code)]
 #![crate_type = "staticlib"]
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
+mod libnx;
+use libnx::*;
 #[no_mangle]
 #[start]
 pub unsafe extern fn main(_argc : isize, _argv : * const * const u8) ->isize {
@@ -33,4 +34,7 @@ pub unsafe extern fn main(_argc : isize, _argv : * const * const u8) ->isize {
     0
 }
 
-pub mod lang_items;
+#[lang = "eh_personality"] pub extern fn eh_personality() {}
+
+use core::panic::PanicInfo;
+#[panic_implementation] #[no_mangle] pub fn panic(_info : &PanicInfo) -> ! { loop{} }
